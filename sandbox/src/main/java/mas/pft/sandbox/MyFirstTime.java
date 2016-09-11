770c5a1ae01a468de2ae623374f6b426f558035a
Logird <regdust@gmail.com>
2016-09-11 21:36:53 +0300

    Вычисление перенесено в метод distance класса Point

--- sandbox/src/main/java/mas/pft/sandbox/MyFirstTime.java
+++ sandbox/src/main/java/mas/pft/sandbox/MyFirstTime.java
@@ -11,10 +11,8 @@ public class MyFirstTime {
     Point b = new Point(-8, -16);
     System.out.println("Координаты точки Б: " + "(" + b.x + ", " + b.y + ").");
     System.out.println();
+    //вычисление и вывод результата
+    System.out.println("Расстояние между точками равно " + a.distance (a, b));
+  }
 
-    System.out.println("Расстояние между точками равно " + distance (a, b));
-  }
-  public static double distance (Point p1, Point p2){
-    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y),2));
-  }
 }
--- sandbox/src/main/java/mas/pft/sandbox/Point.java
+++ sandbox/src/main/java/mas/pft/sandbox/Point.java
@@ -11,4 +11,7 @@ public class Point {
     this.x = x;
     this.y =y;
   }
+  public double distance (Point a, Point b){
+    return Math.sqrt(Math.pow((b.x - a.x), 2) + Math.pow((b.y - a.y),2));
+  }
 }
