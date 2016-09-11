b840584edc7b18e621c029745b84698c1f08ec76
Logird <regdust@gmail.com>
2016-09-11 21:21:45 +0300

    Создан конструктор
    1. Координаты точек задаются через параметры объекта Point(x, y)
    2. Переменная А переименована в а (B -> b)

--- sandbox/src/main/java/mas/pft/sandbox/MyFirstTime.java
+++ sandbox/src/main/java/mas/pft/sandbox/MyFirstTime.java
@@ -5,18 +5,14 @@ public class MyFirstTime {
   public static void main (String[] args){
     System.out.println("Вычисление расстояния между двумя точками в прямоугольной системе координат");
     //первая точка
-    Point A = new Point();
-    A.x = 2.4;
-    A.y = 8;
-    System.out.println("Координаты точки А: " + "(" + A.x + ", " + A.y + ").");
+    Point a = new Point(2.4, 8);
+    System.out.println("Координаты точки А: " + "(" + a.x + ", " + a.y + ").");
     //вторая точка
-    Point B = new Point();
-    B.x = -8;
-    B.y = -16;
-    System.out.println("Координаты точки Б: " + "(" + B.x + ", " + B.y + ").");
+    Point b = new Point(-8, -16);
+    System.out.println("Координаты точки Б: " + "(" + b.x + ", " + b.y + ").");
     System.out.println();
-    
-    System.out.println("Расстояние между точками равно " + distance (A, B));
+
+    System.out.println("Расстояние между точками равно " + distance (a, b));
   }
   public static double distance (Point p1, Point p2){
     return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y),2));
