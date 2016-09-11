1654d0ad315f4d91fe8e38ea6f61ec4fc1556346
Logird <regdust@gmail.com>
2016-09-11 19:14:24 +0300

    Создан класс Point

--- sandbox/src/main/java/mas/pft/sandbox/MyFirstTime.java
+++ sandbox/src/main/java/mas/pft/sandbox/MyFirstTime.java
@@ -3,7 +3,18 @@ package mas.pft.sandbox;
 public class MyFirstTime {
 
   public static void main (String[] args){
-    System.out.println("My First Time");
+    System.out.println("Вычисление расстояния между двумя точками в прямоугольной системе координат");
+    //первая точка
+    Point p1 = new Point();
+    p1.x = 2.4;
+    p1.y = 8;
+    //вторая точка
+    Point p2 = new Point();
+    p2.x = -8;
+    p2.y = -16;
+
+    double len = Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+    System.out.println("Расстояние между точками равно " + len);
   }
   
 }
---
+++ sandbox/src/main/java/mas/pft/sandbox/Point.java
@@ -0,0 +1,9 @@
+package mas.pft.sandbox;
+
+/**
+ * Created by Alexander on 11.09.2016.
+ */
+public class Point {
+  public double x;
+  public double y;
+}
